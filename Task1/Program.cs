c1912ed9818ba753166686fa9e3d3f314f4d85e6
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-13 15:54:30 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int j = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(i, j);
+
+Console.WriteLine(result);
