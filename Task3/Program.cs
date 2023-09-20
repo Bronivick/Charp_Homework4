0b1a7957fd6b84153e3aeebc191bd891d154d944
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-20 18:31:31 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[8];
+GetArray;
+
+void GetArray();
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 500);
+        Console.Write(array[i] + " ");
+    }
+}
